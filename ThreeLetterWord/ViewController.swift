89cb9073ead458859e10fcb4c41fb8780349b570
ARTEAGA,KA KEVIN <rl106856@rlas-macair-20.local>
2018-11-28 10:23:40 -0600

    Initial Commit

---
+++ ThreeLetterWord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThreeLetterWord
+//
+//  Created by ARTEAGA,KA KEVIN on 11/28/18.
+//  Copyright © 2018 ARTEAGA,KA KEVIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
